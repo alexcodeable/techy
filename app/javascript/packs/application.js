3c5c7414a6649ab71632c12888389577ffbf923c
alexcodeable <alexokomado@gmail.com>
2021-05-20 10:02:28 +0100

    initials
    Signed-off-by: alexcodeable <alexokomado@gmail.com>

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,20 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+import "bootstrap"
+
+require("pack/main")
+require("pack/jquery-migrate-3.3.0.min")
+require("pack/modernizr-2.8.3.min")
+require("pack/plugins.min")
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
