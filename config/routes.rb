87a757d3e7e4b7c36aa25867ada9e9461c60abb6
alexcodeable <alexokomado@gmail.com>
2021-05-26 12:23:27 +0100

    added services page and minor fixes left
    Signed-off-by: alexcodeable <alexokomado@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get 'home/blog'
   get 'home/testimonials'
   get 'home/our_history'
+  get 'home/services'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
